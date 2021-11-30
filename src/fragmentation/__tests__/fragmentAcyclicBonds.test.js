38632acf379eb9310d1dcbe276b7d560ea3336ed
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2021-11-30 10:13:25 +0100

    test: test case for acyclic bonds fragmentation

--- src/fragmentation/__tests__/fragmentAcyclicBonds.test.js
+++ src/fragmentation/__tests__/fragmentAcyclicBonds.test.js
@@ -6,8 +6,28 @@ const { Molecule } = OCL;
 
 describe('fragmentAcyclicBonds', () => {
   it('CCO', () => {
-    const molecule = Molecule.fromSmiles('CCO');
+    const molecule = Molecule.fromSmiles('CCCCO');
     const result = fragmentAcyclicBonds(molecule);
-    console.log(result);
+    let testResult = [];
+    for (let i = 0; i < result.length; i++) {
+      testResult[i] = {
+        mf: result[i].mf,
+        monoisotopicMass: result[i].mfInfo.monoisotopicMass,
+        idCode: result[i].idCode,
+      };
+    }
+
+    const fragmentationButanol = [
+      { mf: 'CH3', monoisotopicMass: 15.02347509669, idCode: 'eFBAYc@@' },
+      { mf: 'C3H7O', monoisotopicMass: 59.04968984518, idCode: 'gJQHBEeIVj@@' },
+      { mf: 'C2H5', monoisotopicMass: 29.03912516115, idCode: 'eMBAYRZ@' },
+      { mf: 'C2H5O', monoisotopicMass: 45.03403978072, idCode: 'gCaHLEeIZ`@' },
+      { mf: 'C3H7', monoisotopicMass: 43.05477522561, idCode: 'gC`H@liKT@@' },
+      { mf: 'CH3O', monoisotopicMass: 31.01838971626, idCode: 'eMJDVTf`@' },
+      { mf: 'C4H9', monoisotopicMass: 57.07042529007, idCode: 'gJPH@liJuP@' },
+      { mf: 'HO', monoisotopicMass: 17.0027396518, idCode: 'eFJHVXp@' },
+    ];
+
+    expect(testResult).toStrictEqual(fragmentationButanol);
   });
 });
