ac31365aaf153a351301bb7fa0bdf99c6d1975bd
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2021-12-07 08:35:45 +0100

    fix:fragmentcyclicbonds

--- src/fragmentation/fragmentCyclicBonds.js
+++ src/fragmentation/fragmentCyclicBonds.js
@@ -20,73 +20,92 @@ export function fragmentCyclicBonds(molecule) {
   }
   const fragmentableCyclicBonds = getFragmentableCyclicBonds(molecule);
 
-  let bonds = [];
-
+  let ringsToBeFragmented = [];
   for (let i = 0; i < fragmentableCyclicBonds.length; i++) {
     // get informations of bonds
-    let bond = {};
-    bond.i = fragmentableCyclicBonds[i].i;
+    let ringbond = fragmentableCyclicBonds[i].bond.sort();
 
-    if (
-      fragmentableCyclicBonds.find(
-        (fragmentableCyclicBonds) => fragmentableCyclicBonds === bond.i + 2,
-      )
-    ) {
-      continue;
-    } else {
-      bond.order = molecule.getBondOrder(bond.i); // dative, single , double, triple
-      bond.atom1 = molecule.getBondAtom(0, bond.i); // atom 1 index
-      bond.atom2 = molecule.getBondAtom(1, bond.i); // atom 2 index
-      // we fragment bond 1 and 3
+    for (let s = 0; s < ringbond.length; s++) {
+      for (let r = 0; r < ringbond.length; r++) {
+        let bond = {
+          ring: [],
+          i: [],
+          index: [],
+          order: [],
+          atom1: [],
+          atom2: [],
+          atom3: [],
+          atom4: [],
+          type: [],
+          isAromatic1: [],
+          isAromatic2: [],
+          isRingBond1: [],
+          isRingBond2: [],
+          bond1: [],
+          bond2: [],
+          selected: false,
+        };
+        bond.ring = i;
+        bond.i = ringbond[s];
 
-      bond.atom3 = molecule.getBondAtom(0, bond.i + 2); // atom 3 index
-      bond.atom4 = molecule.getBondAtom(1, bond.i + 2); // atom 4 index
-      bond.type = molecule.getBondType(bond.i); // cBondTypeSingle,cBondTypeDouble,cBondTypeTriple,cBondTypeDelocalized
-      bond.isAromatic1 = molecule.isAromaticBond(bond.i);
-      bond.isAromatic2 = molecule.isAromaticBond(bond.i + 2);
-      bond.isRingBond1 = molecule.isRingBond(bond.i);
-      bond.isRingBond2 = molecule.isRingBond(bond.i + 2);
+        if (bond.i + r + 2 > ringbond.slice(-1)[0]) {
+          continue;
+        } else {
+          bond.order = molecule.getBondOrder(bond.i); // dative, single , double, triple
+          bond.atom1 = molecule.getBondAtom(0, bond.i); // atom 1 index
+          bond.atom2 = molecule.getBondAtom(1, bond.i); // atom 2 index
+          // we fragment bond 1 and 3
+
+          bond.atom3 = molecule.getBondAtom(0, bond.i + r + 2); // atom 3 index
+          bond.atom4 = molecule.getBondAtom(1, bond.i + r + 2); // atom 4 index
+          bond.type = molecule.getBondType(bond.i); // cBondTypeSingle,cBondTypeDouble,cBondTypeTriple,cBondTypeDelocalized
+          bond.isAromatic1 = molecule.isAromaticBond(bond.i);
+          bond.isAromatic2 = molecule.isAromaticBond(bond.i + r + 2);
+          bond.isRingBond1 = molecule.isRingBond(bond.i);
+          bond.isRingBond2 = molecule.isRingBond(bond.i + r + 2);
+
+          bond.bond1 = molecule.getBond(bond.atom1, bond.atom2);
+          bond.bond2 = molecule.getBond(bond.atom3, bond.atom4);
+
+          //    console.log(ringSet.getRingAtoms(ring))
+
+          // Mapping of bonds to be fragmented, only if they are single bond not aromatic and cicly the mapping occurs
+
+          if (
+            bond.isAromatic1 === false &&
+            bond.isAromatic2 === false &&
+            bond.type < 2 &&
+            bond.order !== 0 &&
+            bond.order < 3 &&
+            bond.isRingBond1 &&
+            bond.isRingBond2
+          ) {
+            bond.selected = true;
+            atoms[bond.atom1].links.push(bond.atom2);
+            atoms[bond.atom2].links.push(bond.atom1);
+            atoms[bond.atom3].links.push(bond.atom4);
+            atoms[bond.atom4].links.push(bond.atom3);
+          }
+          ringsToBeFragmented.push(bond);
+        }
+      }
     }
-    bond.bond1 = molecule.getBond(bond.atom1, bond.atom2);
-    bond.bond2 = molecule.getBond(bond.atom3, bond.atom4);
-
-    //    console.log(ringSet.getRingAtoms(ring))
-
-    // Mapping of bonds to be fragmented, only if they are single bond not aromatic and cicly the mapping occurs
-
-    if (
-      bond.isAromatic1 === false &&
-      bond.isAromatic2 === false &&
-      bond.type < 2 &&
-      bond.order !== 0 &&
-      bond.order < 2 &&
-      bond.isRingBond1 &&
-      bond.isRingBond2
-    ) {
-      bond.selected = !bond.selected;
-      atoms[bond.atom1].links.push(bond.atom2);
-      atoms[bond.atom2].links.push(bond.atom1);
-      atoms[bond.atom3].links.push(bond.atom4);
-      atoms[bond.atom4].links.push(bond.atom3);
-    }
-    //// fino a qui funziona ma non riesco a estrarre i dati
-    bonds.push(bond);
   }
+
   let brokenMolecule = {};
   let fragmentMap = [];
   let nbFragments = [];
   let results = [];
 
-  for (let bond of bonds) {
-    if (bond.selected) {
-      // if bond.selected is true (line 46) the molecule will be fragmented
-      brokenMolecule[bond.i] = molecule.getCompactCopy(); // get a copy of the molecule
-      brokenMolecule[bond.i].markBondForDeletion(bond.i); //mark bond to be deleted
-      brokenMolecule[bond.i].markBondForDeletion(bond.i + 2); //mark bond to be deleted
-      brokenMolecule[bond.i].deleteMarkedAtomsAndBonds(bond.i); // delete marked bonds
+  for (let bonds of ringsToBeFragmented) {
+    if (bonds.selected) {
+      brokenMolecule[bonds.i] = molecule.getCompactCopy(); // get a copy of the molecule
+      brokenMolecule[bonds.i].markBondForDeletion(bonds.bond1); //mark bond to be deleted
+      brokenMolecule[bonds.i].markBondForDeletion(bonds.bond2); //mark bond to be deleted
+      brokenMolecule[bonds.i].deleteMarkedAtomsAndBonds(); // delete marked bonds
     }
 
-    nbFragments = brokenMolecule[bond.i].getFragmentNumbers(fragmentMap);
+    nbFragments = brokenMolecule[bonds.i].getFragmentNumbers(fragmentMap);
 
     // only if there are 2 fragments code can continue
     if (nbFragments === 2) {
@@ -100,7 +119,7 @@ export function fragmentCyclicBonds(molecule) {
         let fragment = new Molecule(100, 100);
         let atomMap = [];
 
-        brokenMolecule[bond.i].copyMoleculeByAtoms(
+        brokenMolecule[bonds.i].copyMoleculeByAtoms(
           fragment,
           includeAtom,
           false,
@@ -134,5 +153,6 @@ export function fragmentCyclicBonds(molecule) {
     let result = results[i];
     result.code = String.fromCharCode(65 + i);
   }
+
   return results;
 }
