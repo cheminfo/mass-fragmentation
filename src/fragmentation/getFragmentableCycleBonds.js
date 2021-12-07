361c92037c5a11867310c58553e00e173b3fcdbe
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2021-12-07 14:05:58 +0100

    chore: get fragmentable cyclic bonds function

---
+++ src/fragmentation/getFragmentableCycleBonds.js
@@ -0,0 +1,33 @@
+import { getCyclesAndBondsInfo } from './getCyclesAndBondsInfo';
+/**
+ * This function returns an object with all combination of 2 bonds who can be fragmented in the same cycle
+ * @param {OCL.Molecule} molecule
+ * @returns resuls
+ */
+export function getFragmentableCycleBonds(molecule) {
+  let cycleBondsInfo = getCyclesAndBondsInfo(molecule);
+  let result = [];
+  for (let i = 0; i < cycleBondsInfo.length; i++) {
+    let bonds = cycleBondsInfo[i];
+    let fragmentationBonds = [];
+    let bondLength = bonds.bonds.length;
+    for (let s = 0; s < bondLength; s++) {
+      let end = s < 1 ? bondLength - 1 + s : bondLength;
+      for (let f = s + 2; f < end; f++) {
+        if (
+          bonds.bonds[s].bondOrder === 1 &&
+          bonds.bonds[f].bondOrder === 1 &&
+          !bonds.bonds[s].isAromatic &&
+          !bonds.bonds[f].isAromatic &&
+          bonds.bonds[s].nbCycles < 2 &&
+          bonds.bonds[f].nbCycles < 2
+        ) {
+          fragmentationBonds.push([bonds.bonds[s].bond, bonds.bonds[f].bond]);
+        }
+      }
+    }
+    result.push({ fragmentation: fragmentationBonds });
+  }
+
+  return result;
+}
