6e0f94db08f636716665515a06e0a10f8ed00e7f
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2021-12-07 14:07:06 +0100

    chore: combine cyclic bonds info and fragmentation

---
+++ src/fragmentation/getCycleAndFragmentationInfo.js
@@ -0,0 +1,21 @@
+import { getCyclesAndBondsInfo } from './getCyclesAndBondsInfo';
+import { getFragmentableCycleBonds } from './getFragmentableCycleBonds';
+
+/**
+ * This function returns an array containing bonds informations and all combinations of bonds who can be fragmented
+ * @param {OCL.Molecule} molecule
+ * @returns result
+ */
+export function getCycleAndFragmentationInfo(molecule) {
+  let cycleBonds = getCyclesAndBondsInfo(molecule);
+  let fragmentableBonds = getFragmentableCycleBonds(molecule);
+  let result = [];
+  for (let i = 0; i < cycleBonds.length; i++) {
+    result[i] = {
+      bonds: cycleBonds[i].bonds,
+      fragmentation: fragmentableBonds[i].fragmentation,
+    };
+  }
+
+  return result;
+}
