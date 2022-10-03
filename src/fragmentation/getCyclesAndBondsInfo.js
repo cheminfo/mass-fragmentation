1e28124f6626c95ed660d404ae47ffe0582da9ca
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-10-03 10:35:17 +0200

    docs: correct statement

--- src/fragmentation/getCyclesAndBondsInfo.js
+++ src/fragmentation/getCyclesAndBondsInfo.js
@@ -1,13 +1,13 @@
 /**
- * This function returns ringBond, and object who contains informations about the bonds of each cycle
+ * This function returns ringBond, and object who contains information about the bonds of each cycle
  * @param {OCL.Molecule} molecule
- * @returns {Array} Informations of ring bonds for each cycle in the molecule
+ * @returns {Array} Information of ring bonds for each cycle in the molecule
  */
 
 export function getCyclesAndBondsInfo(molecule) {
   const ringSet = molecule.getRingSet();
   let ringBonds = [];
-
+  // create a new array with the length of the number of bonds in the molecule and fills it with 0
   let nbCycleForBonds = new Array(molecule.getAllBonds()).fill(0);
 
   for (let i = 0; i < ringSet.getSize(); i++) {
