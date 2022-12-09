ea7044e5070e5bd7b3a1ae60bd4a4ed70683f0a9
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-12-09 17:54:03 +0100

    wip: parallel fragmentation benchmark

--- src/parallelFragmention/__tests__/parallelFragment.test.js
+++ src/parallelFragmention/__tests__/parallelFragment.test.js
@@ -1,22 +1,57 @@
+import Benchmark from 'benchmark';
 import OCL from 'openchemlib';
 
+import { fragment } from '../../fragmentation/fragment.js';
 import { parallelFragment } from '../parallelFragment.js';
 
 const { Molecule } = OCL;
-
+// timeout of 10000000
+jest.setTimeout(100000000);
 describe('fragment', () => {
-  it('CC', async () => {
+  it('parallel', async () => {
     const molecule = [
       Molecule.fromSmiles('CC'),
       Molecule.fromSmiles('CCC'),
       Molecule.fromSmiles('C1CCC1'),
       Molecule.fromSmiles('CCCCC2CCC1C(CC)CC(=O)CC1C2'),
       Molecule.fromSmiles('CCCCCC(CC)CCC'),
-    ]; // just ring
+    ];
 
     const results = await parallelFragment(molecule, {
       calculateHoseCodes: true,
     });
     expect(results).toMatchSnapshot();
+  });
+  it('benchMark', async () => {
+    const molecules = [
+      Molecule.fromSmiles('CC'),
+      Molecule.fromSmiles('CCC'),
+      Molecule.fromSmiles('C1CCC1'),
+      Molecule.fromSmiles('CCCCC2CCC1C(CC)CC(=O)CC1C2'),
+      Molecule.fromSmiles('CCCCCC(CC)CCC'),
+      Molecule.fromSmiles('CCCCC2CCC1C(CC)CC(=O)CC1C2'),
+      Molecule.fromSmiles('CC'),
+    ];
+    let result;
+    const suite = new Benchmark.Suite();
+    suite
+      .add('fragment', () => {
+        for (let molecule of molecules) {
+          fragment(molecule, {
+            calculateHoseCodes: true,
+          });
+        }
+      })
+      .add('parallelFragment', async () => {
+        await parallelFragment(molecules, {
+          calculateHoseCodes: true,
+        });
+      })
+      .on('complete', function onComplete() {
+        result = `Fastest is ${this.filter('fastest').map('name')}`;
+      })
+      .run();
+
+    expect(result).toMatchSnapshot();
   });
 });
--- src/parallelFragmention/parallelFragment.js
+++ src/parallelFragmention/parallelFragment.js
@@ -1,8 +1,13 @@
 import { improveFragmentation } from './improveFragmentation';
-
+/**
+ * This function performs the fragmentation process in parallel up to n-1 cores
+ * @param {Array<import('openchemlib').Molecule>} molecules - The an array of OCL molecules to be fragmented
+ * @param {object} [options={}]
+ * @param {boolean} [options.calculateHoseCodes=false] - calculating hose code for bonds is quite time consuming
+ * @returns {Promise} In-Silico fragmentation results
+ */
 export async function parallelFragment(molecules, options = {}) {
   let actions = [];
-  let fragmentationResults = [];
   for (let molecule of molecules) {
     actions.push(improveFragmentation(molecule, options));
   }
